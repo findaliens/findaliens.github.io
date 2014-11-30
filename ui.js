61dfce7f9b9ecfab7d0f7ad7360dbafa6e81f503
findaliens <findaliens@outlook.com>
2014-11-30 10:15:29 +0800

    .

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 function getlist(){
     xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","http://www.findaliens.com/josn/list.json",false);
+    xmlhttp.open("GET","http://www.findaliens.com/json/list.json",false);
     xmlhttp.send();
     xmlhttp.onreadystatechange=function(){
         if (xmlhttp.readyState==4){// 4 = "loaded"
